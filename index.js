fc26475f8d647a4b4ad5688ae65d278f26e8b9ad
edvo <edvoinea@gmail.com>
2018-10-27 11:53:17 +0300

    added webhook

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(3000, () => console.log('Webhook server is listening, port 3000'));
+
+app.listen(3000, () => console.log('Mere'));
